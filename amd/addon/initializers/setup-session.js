a3daa621748c7a95c3c08d28ca815f40d052e0af
Igor Goroshit <igor@webt.co.il>
2015-11-16 03:55:14 +0200

    add default storage to localstorage

--- amd/addon/initializers/setup-session.js
+++ amd/addon/initializers/setup-session.js
@@ -1,4 +1,4 @@
-define('ember-simple-auth/initializers/setup-session', ['exports', 'ember', '../internal-session', '../session-stores/ephemeral', '../utils/inject'], function (exports, _ember, _internalSession, _sessionStoresEphemeral, _utilsInject) {
+define('ember-simple-auth/initializers/setup-session', ['exports', 'ember', '../internal-session', '../session-stores/ephemeral', '../session-stores/local-storage', '../utils/inject'], function (exports, _ember, _internalSession, _sessionStoresEphemeral, _sessionStoresLocalStorage, _utilsInject) {
   'use strict';
 
   exports['default'] = setupSession;
@@ -6,11 +6,11 @@ define('ember-simple-auth/initializers/setup-session', ['exports', 'ember', '../
   function setupSession(registry) {
     registry.register('session:main', _internalSession['default']);
 
-    var store = 'session-store:application';
-    if (_ember['default'].testing) {
-      store = 'session-store:test';
-      registry.register(store, _sessionStoresEphemeral['default']);
-    }
-    (0, _utilsInject['default'])(registry, 'session:main', 'store', store);
+    // let store = LocalStorage;//'session-store:application';
+    // if (Ember.testing) {
+    //   store = 'session-store:test';
+    //   registry.register(store, Ephemeral);
+    // }
+    (0, _utilsInject['default'])(registry, 'session:main', 'store', _sessionStoresLocalStorage['default']);
   }
 });
