101c0518f0c3ed1f9a4f95527ecfb922eff1ed9e
Igor Goroshit <igor@webt.co.il>
2015-11-16 04:02:26 +0200

    fix

--- amd/addon/initializers/setup-session.js
+++ amd/addon/initializers/setup-session.js
@@ -7,11 +7,11 @@ define('ember-simple-auth/initializers/setup-session', ['exports', 'ember', '../
     registry.register('session:main', _internalSession['default']);
 
     var store = 'session-store:application';
-    // if (Ember.testing) {
-    //   store = 'session-store:test';
-    //   registry.register(store, Ephemeral);
-    // }
-    registry.registry('session-store:application', _sessionStoresLocalStorage['default']);
+    if (_ember['default'].testing) {
+      store = 'session-store:test';
+      registry.register(store, _sessionStoresEphemeral['default']);
+    }
+    registry.register(store, _sessionStoresLocalStorage['default']);
     (0, _utilsInject['default'])(registry, 'session:main', 'store', store);
   }
 });
